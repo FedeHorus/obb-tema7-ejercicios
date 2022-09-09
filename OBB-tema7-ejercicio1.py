9e7b4c789e3a0628a0c90d61b732f9da5f698a56
FedeHorus <110682482+FedeHorus@users.noreply.github.com>
2022-09-09 09:20:01 -0300

    Add files via upload

---
+++ OBB-tema7-ejercicio1.py
@@ -0,0 +1,9 @@
+import operaciones as op
+
+
+a,b,c,d=(10,5,20,10)
+    
+print("Suma: ", op.suma(a, b) )
+print("Resta: ",  op.resta(b, d)  )
+print( "Multiplica: ", op.multiplica(b, b)  ) 
+print( "Divide: ", op.divide(a, c) )
